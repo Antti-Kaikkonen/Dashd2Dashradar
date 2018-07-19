84901e0c14387db7da874a9592318279c11b8ff7
Antti Kaikkonen <35118741+Antti-Kaikkonen@users.noreply.github.com>
2018-07-19 18:38:32 +0300

    Greatly improved speed of determining pstype

--- src/main/java/com/dashradar/dashd2dashradar/service/BlockImportServiceImpl.java
+++ src/main/java/com/dashradar/dashd2dashradar/service/BlockImportServiceImpl.java
@@ -17,10 +17,17 @@ import com.dashradar.dashradarbackend.service.BalanceEventService;
 import com.dashradar.dashradarbackend.service.DailyPercentilesService;
 import com.dashradar.dashradarbackend.service.MultiInputHeuristicClusterService;
 import com.dashradar.dashradarbackend.util.TransactionUtil;
+import static com.dashradar.dashradarbackend.util.TransactionUtil.isCollateralOutput;
+import static com.dashradar.dashradarbackend.util.TransactionUtil.isDenomination;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -100,7 +107,7 @@ public class BlockImportServiceImpl implements BlockImportService {
     @Override
     @Transactional
     public void processBlock(BlockDTO block, boolean dayChanged) throws IOException {
-        
+
         if (dayChanged) {
             dayRepository.setLastBlockOfDay(block.getPreviousblockhash());
             for (double percentile = 0.25; percentile < 1; percentile += 0.25) {
@@ -135,7 +142,11 @@ public class BlockImportServiceImpl implements BlockImportService {
                     System.out.println("Transaction "+txid+" moved from orphaned block");
                 } else {
                     TransactionDTO tx = client.getTrasactionByTxId(txid);
-                    transactionRepository.createBlockTransaction(n, tx.getLocktime(), Transaction.PRIVATE_SEND_NONE, tx.getSize(), txid, tx.getVersion(), block.getHash());
+                    
+                    //TODO compute pstype here!
+                    //tx.getVout().forEach(vout -> vout.getValueSat() === TransactionService);
+                    int psType = getPsType(tx);
+                    transactionRepository.createBlockTransaction(n, tx.getLocktime(), psType, tx.getSize(), txid, tx.getVersion(), block.getHash());
                     for (VIn vin : tx.getVin()) {
                         if (vin.getCoinbase() != null) {
                             transactionInputRepository.createCoinbaseInput(txid, vin.getSequence(), vin.getCoinbase());
@@ -155,20 +166,13 @@ public class BlockImportServiceImpl implements BlockImportService {
                     if (n > 0) {
                         transactionRepository.compute_tx_fee(txid);
                         multiInputHeuristicClusterService.clusterizeTransaction(txid);
-
-                        Transaction tx2 = transactionRepository.findByTxid(txid, 2);
-                        int psType = TransactionUtil.getPsType(tx2);
-                        tx2.setPstype(psType);
-                        if (tx2.getPstype() != Transaction.PRIVATE_SEND_NONE) {
-                            transactionRepository.save(tx2);
-                        }
                     }
                 }    
             }
             balanceEventService.createBalances(txid);
             n++;
+            //System.out.println("Fetch: "+totalFetchTime+", Create: "+totalCreateTime+", Fee: "+totalTxFeeTime+", Clusterize: "+totalClusterizeTime+", Balance: "+ totalBalancesTime+", PSType: "+totalPsTypeTime);
         }
-        
         createBlockChainTotals(block);
         createPrivateSendChainTotals(block);
         transactionRepository.create_previous_connections(block.getHash(), Transaction.PRIVATE_SEND_MIXING_100_0);
@@ -188,6 +192,75 @@ public class BlockImportServiceImpl implements BlockImportService {
         transactionRepository.create_mixing_source_connections(block.getHash(), Transaction.PRIVATE_SEND_MIXING_1_0);
         transactionRepository.create_mixing_source_connections(block.getHash(), Transaction.PRIVATE_SEND_MIXING_0_1);
         transactionRepository.create_mixing_source_connections(block.getHash(), Transaction.PRIVATE_SEND_MIXING_0_01);
+    }
+    
+    private int getPsType(TransactionDTO tx) {
+        
+        if (tx.getVin().size() >= 3 && tx.getVin().size() == tx.getVout().size()) {//Possibly mixing
+            long firstValue = tx.getVout().get(0).getValueSat();
+            boolean firstValueIsDenom = TransactionUtil.isDenomination(firstValue);
+
+            boolean allOutputsAreSameDenom = firstValueIsDenom && tx.getVout().stream().skip(1).allMatch(vout -> vout.getValueSat() == firstValue);
+            if (allOutputsAreSameDenom) {
+                boolean mixingTx = allInputsAreSameDenom(firstValue, tx);
+                if (mixingTx) {
+                    if (firstValue == TransactionUtil.DENOM_0_01) {
+                        return Transaction.PRIVATE_SEND_MIXING_0_01;
+                    } else if (firstValue == TransactionUtil.DENOM_0_1) {
+                        return Transaction.PRIVATE_SEND_MIXING_0_1;
+                    } else if (firstValue == TransactionUtil.DENOM_1_0) {
+                        return Transaction.PRIVATE_SEND_MIXING_1_0;
+                    } else if (firstValue == TransactionUtil.DENOM_10_0) {
+                        return Transaction.PRIVATE_SEND_MIXING_10_0;
+                    } else if (firstValue == TransactionUtil.DENOM_100_0) {
+                        return Transaction.PRIVATE_SEND_MIXING_100_0;
+                    }
+                }    
+            }
+        }
+        if (tx.getVout().size() >= 2) {
+            List<Long> nonDenominations = tx.getVout().stream().map(vout -> vout.getValueSat()).filter(valueSat -> !isDenomination(valueSat)).collect(Collectors.toList());
+            if (nonDenominations.size() != tx.getVout().size()) {//at least one denomination output
+                if (nonDenominations.size() == 1) {
+                    return Transaction.PRIVATE_SEND_CREATE_DENOMINATIONS;
+                } else if (nonDenominations.size() == 2) {
+                     if (nonDenominations.stream().anyMatch(denom -> TransactionUtil.isCollateralOutput(denom))) {
+                         return Transaction.PRIVATE_SEND_CREATE_DENOMINATIONS;
+                     }
+                }
+            } 
+        }
+        if (tx.getVout().size() == 1) {//Possibly PrivateSend
+            if (allInputsAreDenoms(tx)) {
+                return Transaction.PRIVATE_SEND;
+            }
+        } 
+        
+        return Transaction.PRIVATE_SEND_NONE;
+    }
+    
+    private boolean allInputsAreDenoms(TransactionDTO tx) {
+        return tx.getVin().stream().allMatch(vin -> {
+            try {
+                TransactionDTO vinTx = client.getTrasactionByTxId(vin.getTxid());
+                VOut spentOutput = vinTx.getVout().stream().filter(vout -> vout.getN() == vin.getVout()).findFirst().get();
+                return TransactionUtil.isDenomination(spentOutput.getValueSat());
+            } catch(Exception ex) {
+                throw new RuntimeException();
+            }
+        });
+    }
+    
+    private boolean allInputsAreSameDenom(long denom, TransactionDTO tx) {
+        return tx.getVin().stream().allMatch(vin -> {
+            try {
+                TransactionDTO vinTx = client.getTrasactionByTxId(vin.getTxid());
+                VOut spentOutput = vinTx.getVout().stream().filter(vout -> vout.getN() == vin.getVout()).findFirst().get();
+                return spentOutput.getValueSat() == denom;
+            } catch(Exception ex) {
+                throw new RuntimeException();
+            }
+        });
     }
 
     @Override
